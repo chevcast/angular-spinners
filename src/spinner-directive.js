0b15a678e7e1a41463e70b3b3074a9b3511ea05d
Alex Ford <alex.ford@codetunnel.com>
2015-06-23 20:36:34 -0600

    Added onShow and onHide options to the spinner directive.

--- src/spinner-directive.js
+++ src/spinner-directive.js
@@ -10,7 +10,9 @@ angular.module('angularSpinners')
         show: '=?',
         imgSrc: '@?',
         register: '@?',
-        onLoaded: '&?'
+        onLoaded: '&?',
+        onShow: '&?',
+        onHide: '&?'
       },
       template: [
         '<span ng-show="show">',
@@ -19,9 +21,14 @@ angular.module('angularSpinners')
         '</span>'
       ].join(''),
       controller: function ($scope, spinnerService) {
+
+        // register should be true by default if not specified.
         if (!$scope.hasOwnProperty('register')) {
           $scope.register = true;
         }
+
+        // Declare a mini-API to hand off to our service so the service
+        // doesn't have a direct reference to this directive's scope.
         var api = {
           name: $scope.name,
           group: $scope.group,
@@ -35,9 +42,25 @@ angular.module('angularSpinners')
             $scope.show = !$scope.show;
           }
         };
+
+        // Register this spinner with the spinner service.
         if ($scope.register) {
           spinnerService._register(api);
         }
+
+        // If an onShow or onHide expression was provided, register a watcher
+        // that will fire the relevant expression when show's value changes.
+        if ($scope.onShow || $scope.onHide) {
+          $scope.$watch('show', function (show) {
+            if (show) {
+              $scope.onShow({ spinnerService: spinnerService, spinnerApi: api });
+            } else {
+              $scope.onHide({ spinnerService: spinnerService, spinnerApi: api });
+            }
+          });
+        }
+
+        // This spinner is good to go. Fire the onLoaded expression.
         $scope.onLoaded({ spinnerService: spinnerService, spinnerApi: api });
       }
     };
