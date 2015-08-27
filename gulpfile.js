be00bdf06c4d676094b0f23db70158c482a8076a
James Black <james@james-black.com>
2015-08-27 11:04:36 -0600

    Some cleanup, and adding a commonjs wrapper to the dist files, this is part of gulp-concat-util now

--- gulpfile.js
+++ gulpfile.js
@@ -7,12 +7,21 @@ gulp.concat = require('gulp-concat');
 gulp.ngAnnotate = require('gulp-ng-annotate');
 gulp.rename = require('gulp-rename');
 gulp.uglify = require('gulp-uglify');
+gulp.concatUtil = require('gulp-concat-util');
 
 gulp.task('build', function () {
   return gulp.src('./src/**/*.js')
     .pipe(gulp.angularFilesort())
     .pipe(gulp.ngAnnotate())
     .pipe(gulp.concat('angular-spinners.js'))
+    .pipe(gulp.concatUtil.header(
+      '/* commonjs package manager support (eg componentjs) */\n' +
+      'if (typeof module !== "undefined" && typeof exports !== "undefined" && module.exports === exports){\n' +
+      '  module.exports = \'angularSpinners\';\n' +
+      '}\n\n' +
+      '(function (window, angular, undefined) {\n'
+    ))
+    .pipe(gulp.concatUtil.footer('})(window, window.angular);'))
     .pipe(gulp.dest('./dist/'))
     .pipe(gulp.uglify())
     .pipe(gulp.rename('angular-spinners.min.js'))
